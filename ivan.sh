b785f3988e41137f0af1e43ff41d6339333d2460
Ivan <ivan@ivanryan.dev>
2022-12-04 21:51:32 +0900

    Update ivan.sh

--- ivan.sh
+++ ivan.sh
@@ -1,4 +1,37 @@
 #!/bin/sh
 
-figlet -f standard "Ivan"
+figlet -f standard "Ivan Ryan"
+
+#print the date
+date
+#print the time
+time
+#print about myself
+print("I am cool")
+sleep 1
+print("I am former military")
+sleep 1
+print("I am a programmer")
+sleep 2
+
+#would you like to know more?
+print("Would you like to know more?")
+sleep 1
+print("Yes or No")
+sleep 1
+#if yes then print more
+if Yes
+then
+print("I am a programmer")
+sleep 1
+print("I am a programmer")
+sleep 1
+print("I am a programmer")
+sleep 2
+
+if No
+then
+print("Goodbye")
+sleep 1
+
 exit
