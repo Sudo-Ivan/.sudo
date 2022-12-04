49d9891e0696a4bbcb5fea5b6dbfcbece6137c9b
Ivan <ivan@ivanryan.dev>
2022-12-04 21:59:15 +0900

    Update ivan.sh

--- ivan.sh
+++ ivan.sh
@@ -11,29 +11,12 @@ sleep 1
 echo "I am former military"
 sleep 1
 echo "I am a programmer"
+sleep 1
+echo "I am a programmer"
+sleep 1
+echo "I am a programmer"
+sleep 1
+echo "Executing virus.exe"
 sleep 2
 
-# would you like to know more?
-echo "Would you like to know more?"
-sleep 1
-echo "Yes or No"
-sleep 1
-
-# if yes then print more
-read response
-if [ "$response" = "Yes" ]; then
-  echo "I am a programmer"
-  sleep 1
-  echo "I am a programmer"
-  sleep 1
-  echo "I am a programmer"
-  sleep 2
-elif [ "$response" = "No" ]; then
-  echo "Goodbye"
-  sleep 1
-  exit
-else
-  echo "Invalid response. Goodbye."
-  sleep 1
-  exit
-fi
+exit
